be343e37c6a85ad67e9d32638f16e8c382208fe9
usmanasif <mail.usmanasif@gmail.com>
2018-05-17 16:58:36 +0500

    Added Name and Manager to serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,11 @@ class User < ActiveRecord::Base
   end
 
   def name
-    self.first_name + self.last_name
+    self.first_name + " " + self.last_name
+  end
+
+  def reports_to
+    User.find(self.reporting_to)
   end
 
   def is_future_joining?
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,21 +1,34 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id,
-            :first_name,
-            :last_name,
-            :username,
-            :email,
-            :company_id,
-            :contact_number,
-            :secondary_contact_number,
-            :emergency_contact_person_name,
-            :emergency_contact_person_number,
-            :emergency_contact_person_relation,
-            :dob,
-            :permanent_address,
-            :temporary_address,
-            :bank_account_number,
-            :employment_history,
-            :performance_evaluation,
-            :image,
-            :reporting_to
+             :first_name,
+             :last_name,
+             :username,
+             :email,
+             :company_id,
+             :contact_number,
+             :secondary_contact_number,
+             :emergency_contact_person_name,
+             :emergency_contact_person_number,
+             :emergency_contact_person_relation,
+             :dob,
+             :permanent_address,
+             :temporary_address,
+             :bank_account_number,
+             :employment_history,
+             :performance_evaluation,
+             :image,
+             :reporting_to,
+             :name,
+             :join_date,
+             :title,
+             :manager,
+             :buddy_id
+  def name
+    object.name
+  end
+
+  def manager
+    object.reports_to.name
+  end
+
 end
