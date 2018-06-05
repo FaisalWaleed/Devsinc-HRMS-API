cd58b1b757a1bd303617762c712ff4fce55421e5
usmanasif <mail.usmanasif@gmail.com>
2018-06-05 16:11:13 +0500

    Refactored and fixed leaves history for user

--- app/helpers/leaves_helper.rb
+++ app/helpers/leaves_helper.rb
@@ -1,19 +1,7 @@
 module LeavesHelper
 
   def get_year_leaves(user)
-    year_leaves_count = 0
-    user.leaves.this_year.each do |leave|
-      year_leaves_count += ( (leave.end_date - leave.start_date).to_i + 1 )
-    end
-    year_leaves_count
-  end
-
-  def get_month_leaves(user)
-    month_leaves_count = 0
-    user.leaves.this_month.each do |leave|
-      month_leaves_count += ( (leave.end_date - leave.start_date).to_i + 1)
-    end
-    month_leaves_count
+    user.leaves.this_year
   end
 
 end
