c219834c675da0fe1e09ac56013d28de0261bd03
usmanasif <mail.usmanasif@gmail.com>
2018-06-06 16:07:07 +0500

    Added method for loading all leaves of specific user * Changed userleave serializer to show leaves by type(previously monthly,yearly leaves were shown)

--- app/controllers/api/v1/leaves_controller.rb
+++ app/controllers/api/v1/leaves_controller.rb
@@ -7,9 +7,10 @@ class Api::V1::LeavesController < ApplicationController
   end
 
   def all_leaves
-    month_leaves = "COALESCE((SELECT SUM(month_details.total_days) FROM (SELECT ((leaves.end_date::date - leaves.start_date::date)+1) as total_days FROM leaves JOIN leave_statuses on leaves.id = leave_statuses.leave_id WHERE leaves.user_id=users.id AND leave_statuses.status='approved' AND leaves.start_date >= date_trunc('month', current_date))month_details ),0) as month_leaves"
-    year_leaves = "COALESCE((SELECT SUM(month_details.total_days) FROM (SELECT ((leaves.end_date::date - leaves.start_date::date)+1) as total_days FROM leaves JOIN leave_statuses on leaves.id = leave_statuses.leave_id WHERE leaves.user_id=users.id AND leave_statuses.status='approved' AND leaves.start_date >= date_trunc('year', current_date ))month_details ),0) as year_leaves"
-    all_user_leaves = User.joins(:leaves).select("users.id,users,first_name,users.last_name,#{month_leaves},#{year_leaves}").group("users.id").to_a
+    sick_leaves = "COALESCE((SELECT SUM(month_details.total_days) FROM (SELECT ((leaves.end_date::date - leaves.start_date::date)+1) as total_days FROM leaves JOIN leave_statuses on leaves.id = leave_statuses.leave_id WHERE leaves.user_id=users.id AND leave_statuses.status='approved' AND leaves.start_date >= date_trunc('year', current_date) AND leaves.leave_type='sick' )month_details ),0) as sick_leaves"
+    annual_leaves = "COALESCE((SELECT SUM(month_details.total_days) FROM (SELECT ((leaves.end_date::date - leaves.start_date::date)+1) as total_days FROM leaves JOIN leave_statuses on leaves.id = leave_statuses.leave_id WHERE leaves.user_id=users.id AND leave_statuses.status='approved' AND leaves.start_date >= date_trunc('year', current_date ) AND leaves.leave_type='annual' )month_details ),0) as annual_leaves"
+    compensation_leaves = "COALESCE((SELECT SUM(month_details.total_days) FROM (SELECT ((leaves.end_date::date - leaves.start_date::date)+1) as total_days FROM leaves JOIN leave_statuses on leaves.id = leave_statuses.leave_id WHERE leaves.user_id=users.id AND leave_statuses.status='approved' AND leaves.start_date >= date_trunc('year', current_date ) AND leaves.leave_type='compensation' )month_details ),0) as compensation_leaves"
+    all_user_leaves = User.joins(:leaves).select("users.id as user_id,users.first_name,users.last_name,#{sick_leaves},#{annual_leaves},#{compensation_leaves}").group("users.id").to_a
     render :json => all_user_leaves, each_serializer: UserLeaveSerializer
   end
 
@@ -33,6 +34,10 @@ class Api::V1::LeavesController < ApplicationController
                    sick: get_year_leaves(user).where(leave_type: "sick").count,
                    compensation: get_year_leaves(user).where(leave_type: "compensation").count,
                }
+  end
+
+  def get_user_leaves
+    render json: User.find(params[:id]).leaves
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -98,6 +98,7 @@ Permission.find_or_create_by(name: "leaves_leave_approvals", description: "Leave
 Permission.find_or_create_by(name: "leaves_create", description: "Create New Leave", group: "Leaves", display_name: "Create new Leave")
 Permission.find_or_create_by(name: "leaves_user_leaves_history", description: "User Leave History", group: "Leaves", display_name: "User Leave History")
 Permission.find_or_create_by(name: "leaves_all_leaves", description: "All Users' Leaves", group: "Leaves", display_name: "All Users' Leave Summary")
+Permission.find_or_create_by(name: "leaves_get_user_leaves", description: "Get Other User Leaves", group: "Leaves", display_name: "Get Other User Leaves")
 
 ############   Module TicketsController  Permissions
 Permission.find_or_create_by(name: "tickets_index", description: "Tickets Index Page", group: "Tickets", display_name: "View Tickets")
